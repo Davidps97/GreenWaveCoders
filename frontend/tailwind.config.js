c53042946ed1eded0deb52b988a7d443ed75f185
David <davidperezsantana1997@gmail.com>
2024-03-11 16:08:37 +0000

    some dev problems fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -36,14 +36,5 @@ export default {
     }
   },
   plugins: [],
-}/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
+
