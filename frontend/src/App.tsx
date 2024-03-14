debf6598e6d2f81f96decffc4912127f641f99bb
kirian <kirianvazquezmoreno@alumno.ieselrincon.es>
2024-03-14 09:53:53 +0000

    last commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "@/pages/SignIn";
 import MyEvent from "@/pages/my-event/MyEvent.tsx";
 import Map from '@/pages/Map/Map.tsx';
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import SignUp from "./pages/SignUp";
 import Event from "./pages/events-page/Event";
+import SignInForm from "@/pages/SignIn";
 
 
 function App() {
     return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/home" element={<Home/>}/>
+      <Route path="/signin" element={<SignInForm/>}/>
         <Route path="/map" element={<Map/>}/>
         <Route path="/myEvent" element={<MyEvent/>}/>
-        <Route path="/" element={<Home/>}/>
         <Route path="/events" element={<Event/>}/>
       </Routes>
     </Router>
